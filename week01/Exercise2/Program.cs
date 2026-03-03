8683481a26771eadfdf9e01ff29137d4052e9ec8
josuezenif <josuezenif@gmail.com>
2026-03-03 17:44:01 -0500

    excercises 1, 2 and 3 coded programs practices

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,39 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("Type in your grade: ");
+        string grade = Console.ReadLine();
+        float studentGrade = int.Parse(grade);
+
+        if (studentGrade >= 90)
+        {
+            string letter = "A";
+            Console.WriteLine($"Congratulations! You passed the class with an {letter}!");
+        }
+
+        else if (studentGrade >= 80 && studentGrade < 90)
+        {
+            string letter = "B";
+            Console.WriteLine($"Congratulations! You passed the class with a {letter}!");
+        }
+
+        else if (studentGrade >= 70 && studentGrade < 80)
+        {
+            string letter = "C";
+            Console.WriteLine($"Congratulations! You passed the class with a {letter}!");
+        }
+
+        else if (studentGrade >= 60 && studentGrade < 70)
+        {
+            string letter = "D";
+            Console.WriteLine($"Sorry, you did not pass the class! You got a {letter} :(");
+        }
+
+        else
+        {
+            string letter = "F";
+            Console.WriteLine($"Sorry, you did not pass! You got an {letter} for FRACASADO!");
+        }
     }
 }
--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,58 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine();
+
+        Console.WriteLine("Welcome to guess my number! Type in a number and we'll say Higher or Lower!");
+        
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+
+        int userGuess = 0;
+        int tries = 0;
+
+        while (userGuess != number)
+        {
+            Console.Write("Type in your guess: ");
+            string guess = Console.ReadLine();
+            userGuess = int.Parse(guess);
+
+            if (userGuess > number)
+            {
+                Console.WriteLine("Lower!");
+            }
+
+            else if (userGuess < number)
+            {
+                Console.WriteLine("Higher!");
+            }
+
+            else if (userGuess == number)
+            {
+                Console.WriteLine("Great job! You guessed the number correctly!");
+                Console.WriteLine();
+            }
+
+            tries += 1;
+        }
+
+        if (tries > 5)
+        {
+            Console.WriteLine($"You took {tries} tries to guess correctly!");
+            Console.WriteLine("Can you guess in less than 5 tries?");
+        }
+        
+        else if (tries < 5)
+        {
+            Console.WriteLine($"Great job! It took you {tries} tries to answer correctly!");
+            Console.WriteLine("You beat the challenge of guessing in less than 5 tries!");
+        }
+
+        
     }
 }
