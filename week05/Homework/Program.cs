048768b978dae867e137ae15db437099c1e14c91
josuezenif <josuezenif@gmail.com>
2026-03-31 19:28:54 -0400

    inherited classes excercice

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,5 +5,22 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+        Console.WriteLine();
+
+        Assignment assignment1 = new Assignment();
+        assignment1.SetStudentName("Josue Zenif");
+        assignment1.GetStudentName();
+
+        assignment1.SetTopic("Web Development");
+        assignment1.GetTopic();
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment math = new MathAssignment("Josue Zenif", "Web Development", "Section 7.4", "Problems 12-20");
+        Console.WriteLine(math.GetHomeworkList());
+
+        WritingAssignment writing = new WritingAssignment("Josue Zenif", "Web development", "Programming with classes");
+        Console.WriteLine(writing.GetWritingInformation());
+
+        Console.WriteLine();
     }
 }
